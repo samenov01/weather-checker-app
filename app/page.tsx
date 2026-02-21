d02d02e63a4b962312b10b13e2a81706fd52c5b8
Саменов Диас <diasss@MacBook-Air-4.local>
2026-02-21 06:17:14 +0500

    adding information about city, county | redesign input and submit bar

--- app/page.tsx
+++ app/page.tsx
@@ -36,31 +36,33 @@ export default function WeatherSearch() {
 
   return (
     <main className='main font-inter'>
-      <p style={{fontSize: '3rem',fontWeight: '900', paddingBottom: '20px'}}>weather checker app.</p>
+      <p style={{fontSize: '3rem',fontWeight: '900', paddingBottom: '20px'}}>weather checker app</p>
       
       <form onSubmit={fetchWeather} className='weather-form'>
-        <input
-          type="text"
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
-          placeholder="Введите город"
-        />
-        <button 
+        <div className="input-form">
+          <input
+            type="text"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            placeholder="Введите город"
+          />
+          <button 
           type="submit" 
           disabled={loading}
           className={'button-center'}
-        >
-        {loading ? 
-          <div className='svg-icons'>
-            <img src="https://seekicon.com/free-icon-download/loading-3-quarters_1.svg" alt="loading" className='loading-icon svg-icons'/>
-          </div>
-        :
-          <div className='svg-icons'>
-            <svg focusable="false" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path></svg>
-          </div>
-        }
-        
-        </button>
+          >
+          {loading ? 
+            <div className='svg-icons'>
+              <img src="https://seekicon.com/free-icon-download/loading-3-quarters_1.svg" alt="loading" className='loading-icon svg-icons'/>
+            </div>
+          :
+            <div className='svg-icons'>
+              <svg focusable="false" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path></svg>
+            </div>
+          }
+          
+          </button>
+        </div>
       </form>
 
       {weatherData && (console.log(weatherData))}
@@ -88,6 +90,13 @@ export default function WeatherSearch() {
               <p className='info-p'>Ветер: {weatherData.wind.speed} м/с</p>
               <p className='info-p'>Состояние: {weatherData.weather[0].description}</p>
             </div>
+          </div>
+          <div className='other-info'>
+              <span className='about'>
+                <div className='about-text-main'>{weatherData.name}</div>
+                <div className='about-text-small'>{weatherData.sys.country}</div>
+                <div className='about-text-small'>{new Date().toLocaleTimeString()}</div>
+              </span>
           </div>
         </div>
       )}
